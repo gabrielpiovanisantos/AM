52f3fb8582d059154b06f2ef2121a5aa5f9f07d5
gabrielpiovanisantos <gabriel.santos@dcomp.sor.ufscar.br>
2018-05-27 16:32:50 -0300

    update readData

--- readData.m
+++ readData.m
@@ -1,8 +1,14 @@
 function D = readData(file)
   fl = fopen(file);
-  D = str2double(strsplit(fgetl(fl), ','));
-  
-  while((line = fgetl(fl)) ~= -1)
+  line = fgetl(fl);
+  while(line ~= "@data")
+    line = fgetl(fl);
+  end
+  line = fgetl(fl);
+  D = str2double(strsplit(line, ','));
+  line = fgetl(fl);
+  while(line ~= -1)
       D = [D; str2double(strsplit(line, ','))];
+      line = fgetl(fl);
   end
 end
