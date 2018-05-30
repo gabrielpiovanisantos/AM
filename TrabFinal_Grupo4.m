d915596ed2a2e1a8b173318a51bcee6d81929908
CG-SS <criguiss@uol.com.br>
2018-05-30 17:42:16 -0300

    Script para realizar todos os passos
    Esse script vai servir como o main do projeto, realizando todos os passos do projeto

---
+++ TrabFinal_Grupo4.m
@@ -0,0 +1,20 @@
+clear; close all; clc;
+
+%year1 = readData('1year.arff');
+%year2 = readData('2year.arff');
+%year3 = readData('3year.arff');
+%year4 = readData('4year.arff');
+%year5 = readData('5year.arff');
+
+load('year1.mat');
+load('year2.mat');
+load('year3.mat');
+load('year4.mat');
+load('year5.mat');
+conjuntoDados = [year1; year2; year3; year4; year5];
+clear year1 year2 year3 year4 year5;
+rotulos = conjuntoDados(:, 65);
+conjuntoDados(:, 65) = [];
+
+[conjuntoDados, conjuntoDados2] = preprocessaDados(conjuntoDados);
+
