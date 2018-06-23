e487110519eb7ab8225fc388c93de513133642b8
CG-SS <criguiss@uol.com.br>
2018-06-23 16:38:13 -0300

    Add files via upload

--- TrabFinal_Grupo4.m
+++ TrabFinal_Grupo4.m
@@ -1,8 +1,9 @@
 clear; close all; clc;
 % Se carregaArq for 1 entao ele carrega os arquivos ja processados
 % previamente, senao ele le os arquivos do 0, a partir dos arquivos .arff
-% para realmente refazer todos os passos do 0.
-carregaArq = 1;
+% para realmente refazer todos os passos do 0, se carregaArq for 2, ele
+% carrega os dados preprocessados e nao realiza nenhum preprocessamento.
+carregaArq = 2;
 
 % Variaveis que dizem quais testes serao rodados. Utilizado caso
 % queira testar apenas um tipo de classificador ao rodar os testes. Se tudo
@@ -24,35 +25,43 @@ if(carregaArq == 0)
     year3 = readData('3year.arff');
     year4 = readData('4year.arff');
     year5 = readData('5year.arff');
-else
+elseif(carregaArq == 1)
     load('year1.mat');
     load('year2.mat');
     load('year3.mat');
     load('year4.mat');
     load('year5.mat');
+else
+    load('year1completo.mat');
+    load('year2completo.mat');
+    load('year3completo.mat');
+    load('year4completo.mat');
+    load('year5completo.mat');
 end
 
-year1rotulos = year1(:, 65);
-year1(:, 65) = [];
-year2rotulos = year2(:, 65);
-year2(:, 65) = [];
-year3rotulos = year3(:, 65);
-year3(:, 65) = [];
-year4rotulos = year4(:, 65);
-year4(:, 65) = [];
-year5rotulos = year5(:, 65);
-year5(:, 65) = [];
- 
-disp('Processando conjunto ano 1.');
-[year1conjunto, year1AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
-disp('Processando conjunto ano 2.');
-[year2conjunto, year2AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
-disp('Processando conjunto ano 3.');
-[year3conjunto, year3AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
-disp('Processando conjunto ano 4.');
-[year4conjunto, year4AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
-disp('Processando conjunto ano 5.');
-[year5conjunto, year5AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
+if(carregaArq < 2)
+    year1rotulos = year1(:, 65);
+    year1(:, 65) = [];
+    year2rotulos = year2(:, 65);
+    year2(:, 65) = [];
+    year3rotulos = year3(:, 65);
+    year3(:, 65) = [];
+    year4rotulos = year4(:, 65);
+    year4(:, 65) = [];
+    year5rotulos = year5(:, 65);
+    year5(:, 65) = [];
+
+    disp('Processando conjunto ano 1.');
+    [year1conjunto, year1AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
+    disp('Processando conjunto ano 2.');
+    [year2conjunto, year2AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
+    disp('Processando conjunto ano 3.');
+    [year3conjunto, year3AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
+    disp('Processando conjunto ano 4.');
+    [year4conjunto, year4AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
+    disp('Processando conjunto ano 5.');
+    [year5conjunto, year5AposNorm] = preprocessaDados(year1, aNorm, aKnn, aReg);
+end
 
 realizaTestes(year1conjunto, year1rotulos, rKnn, rRna, rSVM, rRegLog);
 realizaTestes(year2conjunto, year2rotulos, rKnn, rRna, rSVM, rRegLog);
